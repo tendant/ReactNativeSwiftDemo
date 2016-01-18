b3ab06a247c5670950390d34563f3098bad94429
Lei Wang <tendant@gmail.com>
2016-01-18 00:12:55 -0800

    Add ReactNativeSwiftDemo-Bridging-Header.h

---
+++ ReactNativeSwiftDemo/ReactNativeSwiftDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ReactNativeSwiftDemo-Bridging-Header.h
+//  ReactNativeSwiftDemo
+//
+//  Created by Lei on 1/18/16.
+//  Copyright © 2016 Lei. All rights reserved.
+//
+
+#ifndef ReactNativeSwiftDemo_Bridging_Header_h
+#define ReactNativeSwiftDemo_Bridging_Header_h
+
+#import "RCTRootView.h"
+
+#endif /* ReactNativeSwiftDemo_Bridging_Header_h */
